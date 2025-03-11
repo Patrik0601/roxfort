c901ab84237ce0667c676d792b17c52ca177f53c
Patrik0601 <apatrik0601@gmail.com>
2025-03-11 09:36:39 +0100

    2

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.json())
 app.get('/wizards', (req, res) => {
     res.status(200).json(wizards)
 })
+
 app.get('/wizards/:id', (req, res) => {
     const id = req.params.id
     if(id < 0 || id >= wizards.length){
@@ -15,9 +16,38 @@ app.get('/wizards/:id', (req, res) => {
     }
     res.status(200).json(wizards[id])
 })
-app.post('/wizards', (req, res) => {})
-app.put('/wizards/:id', (req, res) => {})
-app.delete('/wizards/:id', (req, res) => {})
+
+app.post('/wizards', (req, res) => {
+    const {name, wand, house} = req.body
+    if (!name || !wand || !house){
+        return res.status(400).json({message: 'Missing data'})
+    }
+    const newWizard = {name, wand, house}
+    wizards.push(newWizard)
+    res.status(201).json(newWizard)
+})
+
+app.put('/wizards/:id', (req, res) => {
+    const id = req.params.id
+    if(id < 0 || id >= wizards.length){
+        return res.status(404).json({message: 'Wizard not found'})
+    }
+    const {name, wand, house} = req.body
+    if (!name || !wand || !house){
+        return res.status(400).json({message: 'Missing data'})
+    }
+    wizards[id] = {name, wand, house}
+    res.status(200).json(wizards[id])
+})
+
+app.delete('/wizards/:id', (req, res) => {
+    const id = req.params.id
+    if(id < 0 || id >= wizards.length){
+        return res.status(404).json({message: 'Wizard not found'})
+    }
+    wizards.splice(id, 1)
+    res.status(200).json({message:'Delete successful'})
+})
 
 app.listen(3000, ()=>{
     console.log(`Server runs`)
